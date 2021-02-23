76d95b73783da1e1fbf962f46a1b4b430c3918eb
desiramadhani715 <desiramadhani715@gmail.com>
2021-02-23 19:36:00 +0700

    ci movie

---
+++ js/script.js
@@ -0,0 +1,84 @@
+function SearchMovie() {
+    $('#movie-list').html("");
+
+    $.ajax({
+        url: 'https://omdbapi.com',
+        type: 'get',
+        dataType: 'json',
+        data: {
+            'apikey': '2376b5a2',
+            's': $('#search-input').val()
+        },
+        success: function(result) {
+            if (result.Response == "True") {
+                let movies = result.Search;
+                $.each(movies, function(i, data) {
+                    $('#movie-list').append(`
+                        <div class="col-md-4" >
+                            <div class="card mb-3" style="width: 18rem;">
+                                <img src="` + data.Poster + `" class="card-img-top" >
+                                <div class="card-body">
+                                <h5 class="card-title">` + data.Title + `</h5>
+                                <h6 class="card-subtitle mb-2 text-muted">` + data.Year + `</h6>
+                                <a href="#" class="card-link see-detail" data-bs-toggle="modal" data-bs-target="#exampleModal" data-id="` + data.imdbID + `">See Detail</a>
+                                </div>
+                            </div>
+                        </div>
+                    `);
+                });
+                $('#search-input').val('');
+            } else {
+                $('#movie-list').html(`
+                    <div class="col">
+                        <h1 class="text-center">` + result.Error + `</h1>
+                    </div>
+                `);
+            }
+        }
+    });
+
+}
+
+$('#search-button').on('click', function() {
+    SearchMovie();
+});
+
+$('#search-input').on('keyup', function(event) {
+    if (event.which === 13) {
+        SearchMovie();
+    }
+});
+
+$('#movie-list').on('click', '.see-detail', function() {
+    $.ajax({
+        url: 'https://omdbapi.com',
+        type: 'get',
+        dataType: 'json',
+        data: {
+            'apikey': '2376b5a2',
+            'i': $(this).data('id')
+        },
+        success: function(movie) {
+            if (movie.Response === "True") {
+                $('.modal-body').html(`
+                    <div class="container-fluid">
+                        <div class="row">
+                            <div class="col-md-4">
+                                <img src="` + movie.Poster + `" class="img-fluid">
+                            </div>
+                            <div class="col-md-8">
+                                <ul class="list-group">
+                                    <li class="list-group-item"><h4>` + movie.Title + `</h4></li>
+                                    <li class="list-group-item">Released : ` + movie.Released + `</li>
+                                    <li class="list-group-item">Genre : ` + movie.Genre + `</li>
+                                    <li class="list-group-item">Director : ` + movie.Director + `</li>
+                                    <li class="list-group-item">Actors : ` + movie.Actors + `</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                `)
+            }
+        }
+    });
+});
